d2e2e08b5cda7dc969bc214927ef5e676d59381e
Kunsirat Kanchanaphasit <ganis.dyp@gmail.com>
2019-09-09 22:53:30 +0700

    Modify mini logo to SBS

--- backend/themes/adminlte/views/layouts/header.php
+++ backend/themes/adminlte/views/layouts/header.php
@@ -6,7 +6,7 @@ use common\models\Profile;
 ?>
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">DC</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini">SBS</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
