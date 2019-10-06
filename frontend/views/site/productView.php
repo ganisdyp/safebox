54e98ab21bfb33c20ed4d9b5e1a3cb3b2a8fd030
Apichaya Damrongkiattivej <apichayadp@gmail.com>
2019-10-06 14:06:21 +0700

    frontend - update product view and blog category

--- frontend/views/site/productView.php
+++ frontend/views/site/productView.php
@@ -18,32 +18,29 @@ define('PAGE_NAME', 'product');
 <div id="product-page" class="container">
     <nav class="mt-2">
         <ol class="breadcrumb smaller-90 mb-2">
-            <li class="breadcrumb-item"><a href="/site/index"><?php echo Yii::t('common', 'Home');?></a></li>
-            <li class="breadcrumb-item"><a href="/site/product-category"><?php echo Yii::t('common', 'Product');?></a></li>
-            <li class="breadcrumb-item bold"><a href="/site/product-list?id=<?= $product->productType->id; ?>&c=all"><?= $product->productType->name; ?></a>
+            <li class="breadcrumb-item"><a href="<?php echo Yii::$app->request->BaseUrl.'/site/index'; ?>"><?php echo Yii::t('common', 'Home');?></a></li>
+            <li class="breadcrumb-item"><a href="<?php echo Yii::$app->request->BaseUrl.'/site/product-category'; ?>"><?php echo Yii::t('common', 'Product');?></a></li>
+            <li class="breadcrumb-item bold"><a href="/site/product-category?id=<?php echo $product->productType->id; ?>"><?= $product->productType->name; ?></a>
             </li>
-            <li class="breadcrumb-item active"><?= $product->name; ?></li>
+            <li class="breadcrumb-item active"><?php echo $product->name; ?></li>
         </ol>
     </nav>
     <div class="clearfix"></div>
+    <?php /*
     <div class="row">
-        <div class="col-12">
-            <p class="bigger-160 mb-1 text-purple font-weight-normal"><?= $product->name; ?></p>
-        </div>
-        <?php /*
-        <div class="col-md-6 col-12">
-            <p class="text-muted smaller-90 my-1">
-                // <?php <i class="fa fa-clock-o mr-2"></i> ;?>
-                <span>Updated on: </span>
-                <?php
-                $date_publish = date_create($product->date_published);
+      <div class="col-md-6 col-12">
+        <p class="text-muted smaller-90 my-1">
+          // <?php <i class="fa fa-clock-o mr-2"></i> ;?>
+          <span>Updated on: </span>
+          <?php
+          $date_publish = date_create($product->date_published);
 
-                ?>
-                <?= date_format($date_publish, "D, j M Y h:i A") ?>
-            </p>
-        </div>
-        */ ?>
+          ?>
+          <?= date_format($date_publish, "D, j M Y h:i A") ?>
+        </p>
+      </div>
     </div>
+    */ ?>
     <div class="row mt-3 mb-5">
         <div class="col-lg-5 col-12">
             <?php $media_type = $product->media_type;
@@ -74,6 +71,7 @@ define('PAGE_NAME', 'product');
             </div>
         </div>
         <div class="col-lg-7 col-12">
+            <p class="bigger-160 mb-1 text-purple font-weight-normal"><?php echo $product->name; ?></p>
             <div class="row mb-2">
                 <div class="col-md-6 col-12">
                     <p class="text-muted smaller-90 my-1">
@@ -108,8 +106,8 @@ define('PAGE_NAME', 'product');
             <p class="text-muted smaller-90 my-1">
             </p>
         </div>
+        <?php /*
         <div class="col-12 text-center mt-4 pagination-product">
-        <?php
         $dataProvider2 = $searchModel->search(Yii::$app->request->queryParams);
         $all_products = $dataProvider2->query->where(['product_type_id' => $product->productType->id])->all();
         $item_count = 1;
@@ -122,7 +120,7 @@ define('PAGE_NAME', 'product');
             echo "<span title='' class='badge badge-light ".$active."' style='font-size:11pt; font-weight:normal;'><a href='/site/product-view?id=$sc->id'>".$item_count."</a></span> ";
             $item_count++;
         }
-        ?>
         </div>
+        */ ?>
     </div>
 </div>
